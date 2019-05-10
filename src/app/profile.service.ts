d44953a1304b6e4fd884549d67ce347ae1bc429b
ewanyama2019 <wafulawanyama@gmail.com>
2019-05-10 11:04:47 +0300

    added Imports of HttpClient and Map and added in the ecports the Username and secret properties, then adde new function to call API in the profile.service.ts

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private username: string;
+  private clientsecret = '5be2bacf98ee90698772aa913d1509f165ff0ee3';
+
+  constructor(private http: HttpClient) {
+    console.log('Service is now ready');
+    this.username = 'ewanyama2019';
+  }
+
+  getProfileInfomation() {
+    return this.http.get("https://api.github.com/users/ewanyama2019");
+  }
 }
